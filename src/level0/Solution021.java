35c730d47728600e54b9f136160ac3b02e6117e3
BB\lbr41 <lbor3280@naver.com>
2023-11-30 14:05:26 +0900

    level0 Solution.java

---
+++ src/level0/Solution021.java
@@ -0,0 +1,43 @@
+package level0;
+
+public class Solution021 {
+	public static String solution(String code) {
+        String answer = "";
+        char arr[] = new char[code.length()];
+        int mode = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+        	if(code.charAt(i) == '1') {
+        		if(mode == 0) {
+        			mode = 1;
+        		} else {
+        			mode = 0;
+        		}
+        	}
+        	if(mode == 0) {
+        		if(i%2 == 0) {
+        			if(code.charAt(i) != '1') {
+        				answer = answer + code.charAt(i);
+        			}
+        		}
+        	} else {
+        		if(i%2 == 1) {
+        			if(code.charAt(i) != '1') {        				
+        				answer = answer + code.charAt(i);
+        			}
+        		}
+        	}
+        	arr[i] = code.charAt(i);
+        }
+        
+        if(answer.equals("")) {
+        	answer = "EMPTY";
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String args[]) {
+		solution("abc1abc1abc");
+	}
+}
